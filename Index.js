63038a3ee38794ffc3ec2db1cb759086ee3ac884
Daniel Gomez R <ddgomez@uninorte.edu.co>
2020-07-12 10:14:23 -0500

    Se agrego la configuracion del index.js y el express

---
+++ Index.js
@@ -0,0 +1,10 @@
+// Importar Espress
+const express = require('express');
+//Instanciar Express en el objeto app
+const app = express();
+
+
+
+app.listen(3000, () =>{ 
+    console.log("Servidor Iniciado");
+});
