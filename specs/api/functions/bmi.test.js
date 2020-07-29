3482f2091c8022cc2e59b63f3dc4cb8bfd35d0d7
Efren Ospino <efrenospinojr@gmail.com>
2020-07-29 17:14:51 -0500

    Moviendo bmi tests al folder correspondiente

--- specs/api/functions/bmi.test.js
+++ specs/api/functions/bmi.test.js
@@ -1,16 +1,10 @@
-const bmi = require('./../api/functions/bmi');
+const bmi = require('../../../api/functions/bmi');
 
 describe('BMI', () => {
     it('bmi when success', () => {
         const height = 1.8;
         const weight = 80;
         const result = 24.691358024691358;
-        expect(bmi(weight, height)).toBe(result);
-    });
-    it('bmi when error', () => {
-        const height = 0;
-        const weight = 80;
-        const result = -1;
         expect(bmi(weight, height)).toBe(result);
     });
 });
