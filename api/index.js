1ef7cbbc74106edf791ea01bc1d9ad4a0e5ca1ac
Daniel Gomez R <ddgomez@uninorte.edu.co>
2020-07-29 21:25:45 -0500

    5 punto del taller

--- api/index.js
+++ api/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const users = require('./routers/users')
-const tweets = require('./routers/tweets')
+const tweets = require('./routers/tweets') 
 
 router.get('/', (req, res) => {
     res.send('API');
 });
 
 router.use('/users', users);
-router.use('/tweets', tweets);
+router.use('/tweets', tweets); 
+
+
 
 module.exports = router;
